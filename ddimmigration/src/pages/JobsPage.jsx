d3a9058e1e6270ff503b8e9070a60b90552a80d2
ziyu562 <liuziyu7899@icloud.com>
2026-03-11 14:55:12 +1300

    Update JobsPage.jsx

--- ddimmigration/src/pages/JobsPage.jsx
+++ ddimmigration/src/pages/JobsPage.jsx
@@ -8,6 +8,7 @@ function JobsPage() {
     { id: 'construction', label: '建筑类' },
     { id: 'catering', label: '餐饮类' },
     { id: 'factory', label: '工厂类' },
+    { id: 'other', label: '其他类' },
   ]
 
   const jobsByCategory = {
@@ -427,27 +428,27 @@ function JobsPage() {
         living: '食宿自理',
         leave: '做满 1 年有 4 周带薪年假，10天带薪病假',
       },
-      {
-        id: 21,
-        title: '英文好的女性销售人员',
-        requirements: [
-          '形象谈吐良好，擅长销售社交',
-        ],
-        salary: '25 纽币/小时税前起，看能力定薪',
-        visa: 'AEWV 5年工签',
-        living: '食宿自理',
-        leave: '做满 1 年有 4 周带薪年假，10天带薪病假',
-      },
-      {
-        id: 23,
-        title: '按摩师',
-        requirements: ['有按摩、理疗相关经验', '工时 40 小时/周起', '有提成'],
-        salary: '25 纽币/小时起',
-        visa: 'AEWV 5年工签',
-        living: '食宿自理',
-        leave: '做满 1 年有 4 周带薪年假，10天带薪病假',
-      },
     ],
+    other: [{
+      id: 21,
+      title: '英文好的女性销售人员',
+      requirements: [
+        '形象谈吐良好，擅长销售社交',
+      ],
+      salary: '25 纽币/小时税前起，看能力定薪',
+      visa: 'AEWV 5年工签',
+      living: '食宿自理',
+      leave: '做满 1 年有 4 周带薪年假，10天带薪病假',
+    },
+    {
+      id: 23,
+      title: '按摩师',
+      requirements: ['有按摩、理疗相关经验', '工时 40 小时/周起', '有提成'],
+      salary: '25 纽币/小时起',
+      visa: 'AEWV 5年工签',
+      living: '食宿自理',
+      leave: '做满 1 年有 4 周带薪年假，10天带薪病假',
+    },],
   }
 
   const currentJobs = jobsByCategory[selectedCategory] || []
