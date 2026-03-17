3b591599f5b79f5f5a3325ee27339d2c33113909
ziyu562 <liuziyu7899@icloud.com>
2026-03-17 15:32:08 +1300

    u

--- ddimmigration/src/data/albumData.js
+++ ddimmigration/src/data/albumData.js
@@ -3,8 +3,9 @@ export const albumSections = [
   {
     id: 'reception',
     title: '客户接待',
-    cover: '/pic/album/customers/beauty_1773300066804.JPG',
+    cover: '/pic/album/customers/beauty_1773645289185.JPG',
     images: [
+      '/pic/album/customers/beauty_1773645289185.JPG',
       '/pic/album/customers/beauty_1773300066804.JPG',
       '/pic/album/customers/beauty_1773118710877.JPG',
       '/pic/album/customers/beauty_1772144264139 2.JPG',
